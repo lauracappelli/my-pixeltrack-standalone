b65397ae289e54c4bfa0738ca080dacec368bb0a
lauracappelli <laura.cappelli@cnaf.infn.it>
2022-09-12 16:38:18 +0200

    Review testing file for sycl events
    - deleted single add function
    - review variable names
    - reorder output

--- src/sycltest/SYCLCore/testSyclEvents.cc
+++ src/sycltest/SYCLCore/testSyclEvents.cc
@@ -37,29 +37,20 @@ bool check_sum(std::vector<int>& a, std::vector<int>& b)
   return flag;
 }
 
-void single_add(const int *a, const int *b, int *c)
-{
-    *c = *a + *b;
-}
-
 int main() {
   
-  constexpr int N = 16 * 16;
-  constexpr int size = N * sizeof(int);
-  constexpr int threads_per_block = 16;
-
   // create the default queue
   sycl::queue q;
-  std::cout << "Platform: " << q.get_device().get_platform().get_info<sycl::info::platform::name>() << '\n';
+  std::cout << "\nPlatform: " << q.get_device().get_platform().get_info<sycl::info::platform::name>() << '\n';
   
   // create "empty" event
   sycl::event empty_event;
 
-  // simple sum variables
-  int h_a = 2, h_b = 7, h_c;
-  int *d_a, *d_b, *d_c;
+  constexpr int N = 16392 * 16392;
+  constexpr int size = N * sizeof(int);
+  constexpr int threads_per_block = 1024;
 
-  // vector sum variables
+  // vector inizialization
   std::vector<int> h_vec_a, h_vec_b, h_vec_c, h_vec_d;
   int *d_vec_a, *d_vec_b, *d_vec_c;
   h_vec_a.resize(N);
@@ -68,38 +59,23 @@ int main() {
   h_vec_d.resize(N);
   random_fill(h_vec_a);
   random_fill(h_vec_b);
+
   // host vector sum
   sequenzial_sum(h_vec_a, h_vec_b, h_vec_d);
 
   // Allocate space in device memory
-  d_a = (int *)sycl::malloc_device(sizeof(int), q);
-  d_b = (int *)sycl::malloc_device(sizeof(int), q);
-  d_c = (int *)sycl::malloc_device(sizeof(int), q);
   d_vec_a = (int *)sycl::malloc_device(size, q);
   d_vec_b = (int *)sycl::malloc_device(size, q);
   d_vec_c = (int *)sycl::malloc_device(size, q);
 
   // copy data from host to device
-  sycl::event memcpy_event1 = q.memcpy(d_a, &h_a, sizeof(int));
-  sycl::event memcpy_event2 = q.memcpy(d_b, &h_b, sizeof(int));
-  sycl::event memcpy_event4 = q.memcpy(d_vec_a, h_vec_a.data(), size);
-  sycl::event memcpy_event5 = q.memcpy(d_vec_b, h_vec_b.data(), size);
-
-  // simple sum kernel
-  sycl::event device_event = q.submit([&](sycl::handler &h) {
-    h.depends_on(memcpy_event1);
-    h.depends_on(memcpy_event2);
-    h.parallel_for(
-      sycl::nd_range<3>(sycl::range<3>(1, 1, 1), sycl::range<3>(1, 1, 1)),
-      [=](sycl::nd_item<3> item) {
-          single_add(d_a, d_b, d_c);
-      });
-  });
+  sycl::event memcpy_event_vec_a = q.memcpy(d_vec_a, h_vec_a.data(), size);
+  sycl::event memcpy_event_vec_b = q.memcpy(d_vec_b, h_vec_b.data(), size);
 
   // vector sum kernel
-  sycl::event device_event2 = q.submit([&](sycl::handler &h) {
-    h.depends_on(memcpy_event4);
-    h.depends_on(memcpy_event5);
+  sycl::event sum_event = q.submit([&](sycl::handler &h) {
+    h.depends_on(memcpy_event_vec_a);
+    h.depends_on(memcpy_event_vec_b);
     h.parallel_for(
       sycl::nd_range<3>(sycl::range<3>(1, 1, N / threads_per_block) *
       sycl::range<3>(1, 1, threads_per_block), sycl::range<3>(1, 1, threads_per_block)),
@@ -108,76 +84,70 @@ int main() {
     });
   });
 
-  // vector of events that this events waits on
-  auto event_list = device_event.get_wait_list();
-  std::cout << "Size of the event list of device_event " << event_list.size() << '\n';
+  // vector of events that sum_event waits on
+  auto event_list = sum_event.get_wait_list();
+  std::cout << "\t- Size of the event list of sum_event = " << event_list.size() << '\n';
 
-  // copy result from device to host
   // Wait for the associated command to complete
-  device_event.wait();
-  device_event2.wait();
-  sycl::event memcpy_event3 = q.memcpy(&h_c, d_c, sizeof(int));
-  sycl::event memcpy_event6 = q.memcpy(h_vec_c.data(), d_vec_c, size);
-  // Wait for an event to complete, and pass asynchronous errors to handler associated with the command
-  memcpy_event3.wait_and_throw();
-  memcpy_event6.wait_and_throw();
+  sum_event.wait();
 
-  sycl::free(d_a, q);
-  sycl::free(d_b, q);
-  sycl::free(d_c, q);
+  // copy result from device to host and wait for completition passing asynchronous errors to the handler
+  sycl::event memcpy_event_vec_c = q.memcpy(h_vec_c.data(), d_vec_c, size);
+  memcpy_event_vec_c.wait_and_throw();
+  // auto event_list = sum_event.get_wait_list();
+  // q.memcpy(h_vec_c.data(), d_vec_c, size).wait_and_throw(event_list.push_back(sum_event));
+
   sycl::free(d_vec_a, q);
   sycl::free(d_vec_b, q);
   sycl::free(d_vec_c, q);
 
+  if (check_sum(h_vec_c, h_vec_d))
+    std::cout << "\nVector sum correct\n\n";
+  else
+    std::cout << "\nSomething went wrong in vector sum\n\n";
+
+
+  /* 
+  * SYCL EVENTS
+  */
+  std::cout << "EVENT INFORMATIONS\n";
+
+  // vector of events that sum_event waits on
+  // auto event_list = sum_event.get_wait_list();
+  // std::cout << "\t- Size of the event list of sum_event = " << event_list.size() << '\n';
+
+  // Reference count of the event
+  auto ref_count = sum_event.get_info<sycl::info::event::reference_count>();
+  std::cout << "\t- sum_event reference count = " << ref_count << "\n";
+
   // Ask if events are host event
-  std::cout << "empty_event ";
+  std::cout << "\t- The empty_event ";
   if (empty_event.is_host())
     std::cout << "is host event\n";
   else
     std::cout << "is not host event\n";
 
-  std::cout << "memcpy_event ";
-  if (memcpy_event1.is_host())
-    std::cout << "is host event\n";
-  else
-    std::cout << "is not host event\n";
- 
-  // // Wait for vector of events to complete
-  // e.wait(device_event.get_wait_list());
-  // // Wait for a vector of events to complete, and pass asynchronous errors to handlers associated with the commands
-  // e.wait_and_throw(device_event.get_wait_list());
-
-  /*
-   get_info
-  */
-  // Reference count of the event
-  auto ref_count = device_event.get_info<sycl::info::event::reference_count>();
-  std::cout << "device_event reference count " << ref_count << "\n";
   // Command execution status. It could be submitted, running or complete
   if (empty_event.get_info<sycl::info::event::command_execution_status>() == sycl::info::event_command_status::submitted)
-    std::cout << "The empty_event is submitted\n";
+    std::cout << "\t- The empty_event is submitted\n";
   else if (empty_event.get_info<sycl::info::event::command_execution_status>() == sycl::info::event_command_status::running)
-    std::cout << "The empty_event is running\n";
+    std::cout << "\t- The empty_event is running\n";
   else if (empty_event.get_info<sycl::info::event::command_execution_status>() == sycl::info::event_command_status::complete)
-    std::cout << "The empty_event is complete\n";
+    std::cout << "\t- The empty_event is complete\n";
 
-  /*
-    get_profiling_info
-  */
+  std::cout << "\t- sum_event execution time: \n";
+  // get_profiling_info
   // Time in nanoseconds when sycl::command_group was submitted
-  auto submit = device_event2.get_profiling_info<sycl::info::event_profiling::command_submit>();
-  std::cout << "Submit time " << submit / 1.0e9 << " seconds\n";
+  auto submit = sum_event.get_profiling_info<sycl::info::event_profiling::command_submit>();
+  std::cout << "\t  Submit time\t" << submit / 1.0e9 << " seconds\n";
   // Time in nanoseconds when sycl::command_group started execution
-  auto start  = device_event2.get_profiling_info<sycl::info::event_profiling::command_start>();
-  std::cout << "Start time " << start / 1.0e9 << " seconds\n";
+  auto start  = sum_event.get_profiling_info<sycl::info::event_profiling::command_start>();
+  std::cout << "\t  Start time\t" << start / 1.0e9 << " seconds\n";
   // Time in nanoseconds when sycl::command_group finished execution
-  auto end    = device_event2.get_profiling_info<sycl::info::event_profiling::command_end>();
-  std::cout << "End time " << end / 1.0e9 << " seconds\n";
+  auto end    = sum_event.get_profiling_info<sycl::info::event_profiling::command_end>();
+  std::cout << "\t  End time\t" << end / 1.0e9 << " seconds\n";
+  // print results
+  std::cout << "\t  Kernel execution time = " << (end - start) / 1.0e9 << "\n";
+  std::cout << "\t  Total command group processing time = " << (end - submit) / 1.0e9 << "\n\n";
 
-  std::cout << "Kernel execution time = " << (end - start) / 1.0e9 << "\n";
-  std::cout << "Total command group processing time = " << (end - submit) / 1.0e9 << "\n";
-
-  std::cout << "Simple sum - Host result: " << h_a + h_b << ". Device result: " << h_c <<  "\n";
-  if (check_sum(h_vec_c, h_vec_d))
-    std::cout << "Vector sum correct\n";
 }
