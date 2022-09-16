9c763e426a83385f4905e98901ae9c4727eb7ec9
lauracappelli <laura.cappelli@cnaf.infn.it>
2022-09-16 11:56:52 +0200

    Conversion of CachingAllocator.h
    The events are temporarly removed

--- src/sycltest/SYCLCore/CachingAllocator.h
+++ src/sycltest/SYCLCore/CachingAllocator.h
@@ -1,16 +1,16 @@
 #ifndef SYCLCore_CachingAllocator_h
 #define SYCLCore_CachingAllocator_h
 
-// #include <cassert>
-// #include <exception>
-// #include <iomanip>
+#include <cassert>
+#include <exception>
+#include <iomanip>
 #include <iostream>
-// #include <map>
-// #include <mutex>
-// #include <sstream>
-// #include <string>
-// #include <tuple>
-// #include <type_traits>
+#include <map>
+#include <mutex>
+#include <sstream>
+#include <string>
+#include <tuple>
+#include <type_traits>
 
 #include <CL/sycl.hpp>
 
@@ -48,39 +48,47 @@ namespace cms::sycltools {
     }
 
   }  // namespace detail
-
+  
+  /*
+   * The "memory device" identifies the memory space, i.e. the device where the 
+   * memory is allocated.
+   * A caching allocator object is associated to a single memory `Device`, set 
+   * at construction time, and unchanged for the lifetime of the allocator.
+  */
+  
   template <typename TDevice, typename TQueue>
   class CachingAllocator {
   public:
-    using Device = TDevice;              // the "memory device", where the memory will be allocated
-    using Queue = TQueue;                // the queue used to submit the memory operations
-    using Event = alpaka::Event<Queue>;  // the events used to synchronise the operations
-    using Buffer = alpaka::Buf<Device, std::byte, alpaka::DimInt<1u>, size_t>;
-
-    // The "memory device" type can either be the same as the "synchronisation device" type, or be the host CPU.
-    static_assert(std::is_same_v<Device, alpaka::Dev<Queue>> or std::is_same_v<Device, alpaka::DevCpu>,
-                  "The \"memory device\" type can either be the same as the \"synchronisation device\" type, or be the "
-                  "host CPU.");
+    using Device = TDevice; // memory device, where the memory will be allocated
+    using Queue = TQueue;   // the queue used to submit the memory operations
+    using Event = sycl::event;  // the events used to synchronise the operations
 
     struct CachedBytes {
       size_t free = 0;       // total bytes freed and cached on this device
-      size_t live = 0;       // total bytes currently in use oin this device
+      size_t live = 0;       // total bytes currently in use on this device
       size_t requested = 0;  // total bytes requested and currently in use on this device
     };
 
     explicit CachingAllocator(
         Device const& device,
-        unsigned int binGrowth,          // bin growth factor;
-        unsigned int minBin,             // smallest bin, corresponds to binGrowth^minBin bytes;
-                                         // smaller allocations are rounded to this value;
-        unsigned int maxBin,             // largest bin, corresponds to binGrowth^maxBin bytes;
-                                         // larger allocations will fail;
-        size_t maxCachedBytes,           // total storage for the allocator (0 means no limit);
-        double maxCachedFraction,        // fraction of total device memory taken for the allocator (0 means no limit);
-                                         // if both maxCachedBytes and maxCachedFraction are non-zero,
-                                         // the smallest resulting value is used.
-        bool reuseSameQueueAllocations,  // reuse non-ready allocations if they are in the same queue as the new one;
-                                         // this is safe only if all memory operations are scheduled in the same queue
+        unsigned int binGrowth,         // bin growth factor;
+        unsigned int minBin,            // smallest bin, corresponds to 
+                                        // binGrowth^minBin bytes;
+                                        // smaller allocations are rounded to minBin
+        unsigned int maxBin,            // largest bin, corresponds to //
+                                        // binGrowth^maxBin bytes;
+                                        // larger allocations will fail;
+        size_t maxCachedBytes,          // total storage for the allocator 
+                                        // (0 means no limit);
+        double maxCachedFraction,       // fraction of total device memory taken
+                                        // for the allocator (0 means no limit);
+                                        // if both maxCachedBytes and 
+                                        // maxCachedFraction are non-zero,
+                                        // the smallest resulting value is used.
+        bool reuseSameQueueAllocations, // reuse non-ready allocations if they 
+                                        // are in the same queue as the new one;
+                                        // this is safe only if all memory
+                                        // operations are scheduled in the same queue
         bool debug)
         : device_(device),
           binGrowth_(binGrowth),
@@ -128,7 +136,7 @@ namespace cms::sycltools {
     void* allocate(size_t bytes, Queue queue) {
       // create a block descriptor for the requested allocation
       BlockDescriptor block;
-      block.queue = std::move(queue);
+      block.queue = queue;
       block.requested = bytes;
       std::tie(block.bin, block.bytes) = findBin(bytes);
 
@@ -137,7 +145,7 @@ namespace cms::sycltools {
         allocateNewBlock(block);
       }
 
-      return block.buffer->data();
+      return block.d_ptr;
     }
 
     // frees an allocation
@@ -158,7 +166,6 @@ namespace cms::sycltools {
 
       bool recache = (cachedBytes_.free + block.bytes <= maxCachedBytes_);
       if (recache) {
-        alpaka::enqueue(*(block.queue), *(block.event));
         cachedBytes_.free += block.bytes;
         // after the call to insert(), cachedBlocks_ shares ownership of the buffer
         // TODO use std::move ?
@@ -166,22 +173,25 @@ namespace cms::sycltools {
 
         if (debug_) {
           std::ostringstream out;
-          out << "\t" << deviceType_ << " " << alpaka::getName(device_) << " returned " << block.bytes << " bytes at "
-              << ptr << " from associated queue " << block.queue->m_spQueueImpl.get() << " , event "
-              << block.event->m_spEventImpl.get() << " .\n\t\t " << cachedBlocks_.size() << " available blocks cached ("
-              << cachedBytes_.free << " bytes), " << liveBlocks_.size() << " live blocks (" << cachedBytes_.live
-              << " bytes) outstanding." << std::endl;
+          out << "\t" << deviceType_ << " " << device_.get_info<info::device::name>()
+              << " returned " << block.bytes << " bytes at " << ptr  << " from associated queue " 
+              << block.queue.get_info<sycl::info::queue::reference_count>() 
+              << " .\n\t\t " << cachedBlocks_.size() << " available blocks cached ("
+              << cachedBytes_.free << " bytes), " << liveBlocks_.size() << " live blocks (" 
+              << cachedBytes_.live << " bytes) outstanding." << std::endl;
           std::cout << out.str() << std::endl;
         }
       } else {
         // if the buffer is not recached, it is automatically freed when block goes out of scope
         if (debug_) {
           std::ostringstream out;
-          out << "\t" << deviceType_ << " " << alpaka::getName(device_) << " freed " << block.bytes << " bytes at "
-              << ptr << " from associated queue " << block.queue->m_spQueueImpl.get() << ", event "
-              << block.event->m_spEventImpl.get() << " .\n\t\t " << cachedBlocks_.size() << " available blocks cached ("
-              << cachedBytes_.free << " bytes), " << liveBlocks_.size() << " live blocks (" << cachedBytes_.live
-              << " bytes) outstanding." << std::endl;
+          out << "\t" << deviceType_ << " " << device_.get_info<info::device::name>()
+              << " freed " << block.bytes << " bytes at " << ptr << " from associated queue " 
+              << block.queue.get_info<sycl::info::queue::reference_count>()
+              << " .\n\t\t " << cachedBlocks_.size() << " available blocks cached ("
+              << cachedBytes_.free << " bytes), " << liveBlocks_.size() 
+              << " live blocks (" << cachedBytes_.live << " bytes) outstanding." 
+              << std::endl;
           std::cout << out.str() << std::endl;
         }
       }
@@ -189,21 +199,20 @@ namespace cms::sycltools {
 
   private:
     struct BlockDescriptor {
-      std::optional<Buffer> buffer;
-      std::optional<Queue> queue;
-      std::optional<Event> event;
-      size_t bytes = 0;
-      size_t requested = 0;  // for monitoring only
-      unsigned int bin = 0;
+      void *d_ptr;           // Device pointer
+      size_t bytes;          // Size of allocation in bytes
+      size_t bytesRequested; // requested allocatoin size (for monitoring only)
+      unsigned int bin;      // Bin enumeration
+      Queue queue;
+      Event event;
 
       // the "synchronisation device" for this block
-      auto device() { return alpaka::getDev(*queue); }
+      auto device() { return queue.get_device(); }
     };
 
-    // TODO replace with a tbb::concurrent_multimap ?
     using CachedBlocks = std::multimap<unsigned int, BlockDescriptor>;  // ordered by the allocation bin
-    // TODO replace with a tbb::concurrent_map ?
     using BusyBlocks = std::map<void*, BlockDescriptor>;  // ordered by the address of the allocated memory
+    using EventCompleted = sycl::info::event_command_status::complete;
 
     inline static const std::string deviceType_ = boost::core::demangle(typeid(Device).name());
 
@@ -227,8 +236,7 @@ namespace cms::sycltools {
 
     // return the maximum amount of memory that should be cached on this device
     size_t cacheSize(size_t maxCachedBytes, double maxCachedFraction) const {
-      // note that getMemBytes() returns 0 if the platform does not support querying the device memory
-      size_t totalMemory = alpaka::getMemBytes(device_);
+      size_t totalMemory = dev.get_info<info::device::global_mem_cache_size>();
       size_t memoryFraction = static_cast<size_t>(maxCachedFraction * totalMemory);
       size_t size = std::numeric_limits<size_t>::max();
       if (maxCachedBytes > 0 and maxCachedBytes < size) {
@@ -266,22 +274,15 @@ namespace cms::sycltools {
       // iterate through the range of cached blocks in the same bin
       const auto [begin, end] = cachedBlocks_.equal_range(block.bin);
       for (auto iBlock = begin; iBlock != end; ++iBlock) {
-        if ((reuseSameQueueAllocations_ and (*block.queue == *(iBlock->second.queue))) or
-            alpaka::isComplete(*(iBlock->second.event))) {
-          // associate the cached buffer to the new queue
-          auto queue = std::move(*(block.queue));
-          // TODO cache (or remove) the debug information and use std::move()
+        if ((reuseSameQueueAllocations_ and (block.queue == (iBlock->second.queue))) or
+           (iBlock->second.event).get_info<sycl::info::event::command_execution_status>() == EventCompleted) {
           block = iBlock->second;
-          block.queue = std::move(queue);
 
-          // if the new queue is on different device than the old event, create a new event
-          if (block.device() != alpaka::getDev(*(block.event))) {
-            block.event = Event{block.device()};
-          }
+          // SYCL:: here we may create a new event if the new block is on a different queue respect the previous one
 
           // insert the cached block into the live blocks
           // TODO cache (or remove) the debug information and use std::move()
-          liveBlocks_[block.buffer->data()] = block;
+          liveBlocks_[block.d_ptr] = block;
 
           // update the accounting information
           cachedBytes_.free -= block.bytes;
@@ -290,11 +291,13 @@ namespace cms::sycltools {
 
           if (debug_) {
             std::ostringstream out;
-            out << "\t" << deviceType_ << " " << alpaka::getName(device_) << " reused cached block at "
-                << block.buffer->data() << " (" << block.bytes << " bytes) for queue "
-                << block.queue->m_spQueueImpl.get() << ", event " << block.event->m_spEventImpl.get()
-                << " (previously associated with stream " << iBlock->second.queue->m_spQueueImpl.get() << " , event "
-                << iBlock->second.event->m_spEventImpl.get() << ")." << std::endl;
+            out << "\t" << deviceType_ << " " << device_.get_info<info::device::name>() 
+                << " reused cached block at " << block.d_ptr 
+                << " (" << block.bytes << " bytes) for queue "
+                << block.queue.get_info<sycl::info::queue::reference_count>() 
+                << " (previously associated with stream " 
+                << iBlock->second.queue.get_info<sycl::info::queue::reference_count>() 
+                << ")." << std::endl;
             std::cout << out.str() << std::endl;
           }
 
@@ -307,30 +310,26 @@ namespace cms::sycltools {
       return false;
     }
 
-    Buffer allocateBuffer(size_t bytes, Queue const& queue) {
-      if constexpr (std::is_same_v<Device, alpaka::Dev<Queue>>) {
-        // allocate device memory
-        return alpaka::allocBuf<std::byte, size_t>(device_, bytes);
-      } else if constexpr (std::is_same_v<Device, alpaka::DevCpu>) {
+    void* allocateBuffer(size_t bytes, Queue const& queue) {
+      if (q.get_device().is_host()) {
         // allocate pinned host memory
-        return alpaka::allocMappedBuf<std::byte, size_t>(device_, alpaka::getDev(queue), bytes);
+        return sycl::malloc_host(bytes, queue);
       } else {
-        // unsupported combination
-        static_assert(std::is_same_v<Device, alpaka::Dev<Queue>> or std::is_same_v<Device, alpaka::DevCpu>,
-                      "The \"memory device\" type can either be the same as the \"synchronisation device\" type, or be "
-                      "the host CPU.");
+        // allocate device memory
+        return sycl::malloc_host(bytes, queue);
       }
     }
 
     void allocateNewBlock(BlockDescriptor& block) {
       try {
-        block.buffer = allocateBuffer(block.bytes, *block.queue);
-      } catch (std::runtime_error const& e) {
+        block.d_ptr = allocateBuffer(block.bytes, *block.queue);
+      } catch (sycl::exception const& e) {
         // the allocation attempt failed: free all cached blocks on the device and retry
         if (debug_) {
           std::ostringstream out;
-          out << "\t" << deviceType_ << " " << alpaka::getName(device_) << " failed to allocate " << block.bytes
-              << " bytes for queue " << block.queue->m_spQueueImpl.get()
+          out << "\t" << deviceType_ << " " << device_.get_info<info::device::name>()
+              << " failed to allocate " << block.bytes << " bytes for queue " 
+              << block.queue.get_info<sycl::info::queue::reference_count>()
               << ", retrying after freeing cached allocations" << std::endl;
           std::cout << out.str() << std::endl;
         }
@@ -338,25 +337,23 @@ namespace cms::sycltools {
         freeAllCached();
 
         // throw an exception if it fails again
-        block.buffer = allocateBuffer(block.bytes, *block.queue);
+        block.d_ptr = allocateBuffer(block.bytes, *block.queue);
       }
-
-      // create a new event associated to the "synchronisation device"
-      block.event = Event{block.device()};
 
       {
         std::scoped_lock lock(mutex_);
         cachedBytes_.live += block.bytes;
         cachedBytes_.requested += block.requested;
-        // TODO use std::move() ?
-        liveBlocks_[block.buffer->data()] = block;
+        liveBlocks_[block.d_ptr] = block;
       }
 
       if (debug_) {
         std::ostringstream out;
-        out << "\t" << deviceType_ << " " << alpaka::getName(device_) << " allocated new block at "
-            << block.buffer->data() << " (" << block.bytes << " bytes associated with queue "
-            << block.queue->m_spQueueImpl.get() << ", event " << block.event->m_spEventImpl.get() << "." << std::endl;
+        out << "\t" << deviceType_ << " " << device_.get_info<info::device::name>() 
+            << " allocated new block at " << block.d_ptr << " (" << block.bytes 
+            << " bytes associated with queue "
+            << block.queue.get_info<sycl::info::queue::reference_count>() << "." 
+            << std::endl;
         std::cout << out.str() << std::endl;
       }
     }
@@ -370,9 +367,11 @@ namespace cms::sycltools {
 
         if (debug_) {
           std::ostringstream out;
-          out << "\t" << deviceType_ << " " << alpaka::getName(device_) << " freed " << iBlock->second.bytes
-              << " bytes.\n\t\t  " << (cachedBlocks_.size() - 1) << " available blocks cached (" << cachedBytes_.free
-              << " bytes), " << liveBlocks_.size() << " live blocks (" << cachedBytes_.live << " bytes) outstanding."
+          out << "\t" << deviceType_ << " " << device_.get_info<info::device::name>()
+              << " freed " << iBlock->second.bytes << " bytes.\n\t\t  " 
+              << (cachedBlocks_.size() - 1) << " available blocks cached (" 
+              << cachedBytes_.free << " bytes), " << liveBlocks_.size() 
+              << " live blocks (" << cachedBytes_.live << " bytes) outstanding."
               << std::endl;
           std::cout << out.str() << std::endl;
         }
