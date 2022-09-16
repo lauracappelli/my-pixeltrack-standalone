8e94fc81175e3eb94d7ba93c886d93b4df99b440
lauracappelli <laura.cappelli@cnaf.infn.it>
2022-09-16 15:12:15 +0200

    Conversion of getHostCachingAllocator.h

--- src/sycltest/SYCLCore/getHostCachingAllocator.h
+++ src/sycltest/SYCLCore/getHostCachingAllocator.h
@@ -1,8 +1,26 @@
 #ifndef SYCLCore_getHostCachingAllocator_h
 #define SYCLCore_getHostCachingAllocator_h
 
-namespace cms::sycltools {
+#include "SYCLCore/AllocatorConfig.h"
+#include "SYCLCore/CachingAllocator.h"
 
+namespace cms::sycltools {
+  
+  template <typename TDevice, typename TQueue>
+    inline CachingAllocator<TDevice, TQueue>& getHostCachingAllocator() {
+      // thread safe initialisation of the host allocator
+      static CachingAllocator<TDevice, TQueue> allocator (host,
+                                                          config::binGrowth,
+                                                          config::minBin,
+                                                          config::maxBin,
+                                                          config::maxCachedBytes,
+                                                          config::maxCachedFraction,
+                                                          false,   // reuseSameQueueAllocations
+                                                          false);  // debug
+
+      // the public interface is thread safe
+      return allocator;
+    }
 
 }  // namespace cms::sycltools
 
