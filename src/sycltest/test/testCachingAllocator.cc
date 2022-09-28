a883127eb76f42e9bc52020ac76e9e1b670173d7
lauracappelli <laura.cappelli@cnaf.infn.it>
2022-09-28 16:09:18 +0200

    Improve tests

--- src/sycltest/test/testCachingAllocator.cc
+++ src/sycltest/test/testCachingAllocator.cc
@@ -1,6 +1,6 @@
 #include <CL/sycl.hpp>
-#include "CachingAllocator.h"
-#include "AllocatorConfig.h"
+#include "SYCLCore/CachingAllocator.h"
+#include "SYCLCore/AllocatorConfig.h"
 
 #include <vector>
 
@@ -8,6 +8,7 @@ int main() {
     sycl::queue queue = sycl::queue{sycl::cpu_selector()};
     unsigned int N = 100;
     std::vector<int> v(N);
+    std::vector<int> v2(N/2);
 
     cms::sycltools::CachingAllocator cachingAllocator(queue.get_device(),
         cms::sycltools::config::binGrowth,
@@ -16,7 +17,7 @@ int main() {
         cms::sycltools::config::maxCachedBytes,
         cms::sycltools::config::maxCachedFraction,
         true,
-        false);
+        true);
 
     void* ptr = cachingAllocator.allocate(N*sizeof(int), queue);
     int* int_ptr = reinterpret_cast<int*>(ptr);
@@ -27,12 +28,27 @@ int main() {
 
     queue.memcpy(v.data(), ptr, N * sizeof(int));
     queue.wait();
+    
+    // for(auto el:v){
+    //   std::cout << el << ", ";
+    // }
+    // std::cout << std::endl;
+
+    void* ptr2 = cachingAllocator.allocate(N/2*sizeof(int), queue);
+    int* int_ptr2 = reinterpret_cast<int*>(ptr2);
+
+    for (int i=0; i < N/2; i++) {
+      int_ptr2[i] = 42;
+    }
+
+    queue.memcpy(v2.data(), ptr2, N/2* sizeof(int));
+    queue.wait();
 
     cachingAllocator.free(ptr);
-
-    for(auto el:v){
-      std::cout << el << ", ";
-    }
+    cachingAllocator.free(ptr2);
+    
+    // for(auto el:v2){
+    //   std::cout << el << ", ";
+    // }
     std::cout << std::endl;
-
 }
